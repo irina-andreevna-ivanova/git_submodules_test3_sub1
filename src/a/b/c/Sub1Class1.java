a2f6c31a2c3f51d1a7bc8ab60063a2d6fd3ee3a9
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2018-09-11 18:53:54 +0300

    Sub1Class1.java

--- src/a/b/c/Sub1Class1.java
+++ src/a/b/c/Sub1Class1.java
@@ -4,6 +4,7 @@ package a.b.c;
  * *******************************
  * Created by Irina.Petrovskaya on 9/11/2018.
  * Project: git_submodules_test3_sub1
+ *
  * *******************************
  */
 public class Sub1Class1 {
